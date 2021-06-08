8ceb07654fdb2a08bb881731d59621b182c1b21f
sapoepsilon <47342870+sapoepsilon@users.noreply.github.com>
2021-06-07 20:14:37 -0600

    Add .gitignore gile

---
+++ RegisterView.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterView.swift
+//  Hydro Homie
+//
+//  Created by Ismatulla Mansurov on 6/7/21.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
